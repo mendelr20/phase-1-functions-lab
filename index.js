99793e4b45390712739f830ee7314c6630360c3f
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-04-21 13:49:32 -0400

    commit tree 98fb50cf2bd7dbe44dda772a86aedc400c5a80a8

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(bStart){
+    if (bStart > 42)
+        return bStart - 42
+    if (bStart < 42 )
+        return 42 - bStart
+}
+
+function distanceFromHqInFeet(fStart){
+    if (fStart > 42) 
+        return (fStart - 42)*264
+    if (fStart < 42)
+        return (42 - fStart)*264
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start > destination)
+        return (start - destination)*264
+    if (start < destination)
+        return (destination - start)*264
+}
+
+function calculatesFarePrice(start, destination){
+    let blockDistance = Math.abs (start - destination)
+    let distanceTravelled = blockDistance * 264;
+
+    if (distanceTravelled <= 400){
+        console.log('give customers a free sample')
+        return 0
+    }
+
+    else if (distanceTravelled >= 2500)
+        return 'cannot travel that far'
+    else if (distanceTravelled > 2000)
+        return 25
+    else if (400 < distanceTravelled && distanceTravelled < 2001)
+        return (distanceTravelled - 400) * 0.02
+}   
+
+
+
